b30c1dd0bc52a9f0ec92cb2897e3028b064cf4c2
muhammadbilal <ahmad_4al@hotmail.com>
2023-05-11 15:41:44 +0500

    latest code

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -47,7 +47,7 @@ const AuthLayout = () => {
 
   return (
 
-    <div className='row h-100 position-relative'>
+    <div className='row h-100 position-relative m-0'>
 
       <div className='flex flex-column col-xxl-5 col-lg-6 p-10  order-lg-0 order-1 section-1'>
         <div className='d-flex flex-column flex-lg-row-fluid justify-content-center h-100'>
@@ -60,22 +60,24 @@ const AuthLayout = () => {
 
       <div className='col-xxl-7 col-lg-6 pb-8 section-2'>
         <div className=' order-xl-1 order-0 slideshow relative'>
-          <Slide  {...properties}>
+          <Slide  {...properties} >
             {slideImages.map((slideImage, index) => (
               <div className="each-slide " key={index}>
                 <div className='linearGradient rounded-l-3xl bgi-size-cover bgi-position-center lg:!mt-8 lg:!mr-0' style={{ 'backgroundImage': `url(${toAbsoluteUrl(slideImage.url)})` }}>
-                  {/* <div className='d-flex flex-column py-15 px-5 px-md-15 w-100'>
-                    <h1 className='text-white fs-2qx fw-bolder mt-96'>
-                      Lorem ipsum dolor sit amet
-                    </h1>
-                    <div className='text-white fs-base mt-5'>
-                      Lorem ipsum dolor sit amet consectetur. Non sit volutpat egestas tempus
-                      molestie posuere nullam cursus. Egestas venenatis fusce turpis aenean sem
-                      sit bibendum. Libero sit tincidunt dui phasellus adipiscing fermentum molestie urna.
-                      Non consectetur sapien eleifend leo lorem neque sed eget. Enim pellentesque ultrices
-                      nisl sit odio nam nullam cursus. Neque morbi dui purus mattis.
+                  <div className='d-none d-lg-flex justify-content-end flex-column py-15 px-5 px-md-15 w-100 '>
+                    <div className='h-max-content'>
+                      <h1 className='text-white fs-2qx fw-bolder mt-96'>
+                        Lorem ipsum dolor sit amet
+                      </h1>
+                      <div className='text-white fs-base mt-5'>
+                        Lorem ipsum dolor sit amet consectetur. Non sit volutpat egestas tempus
+                        molestie posuere nullam cursus. Egestas venenatis fusce turpis aenean sem
+                        sit bibendum. Libero sit tincidunt dui phasellus adipiscing fermentum molestie urna.
+                        Non consectetur sapien eleifend leo lorem neque sed eget. Enim pellentesque ultrices
+                        nisl sit odio nam nullam cursus. Neque morbi dui purus mattis.
+                      </div>
                     </div>
-                  </div> */}
+                  </div>
                 </div>
               </div>
             ))}
