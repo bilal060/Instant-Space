b4b3b9193c1d989961240fe4720686c11cbde094
bilaldev001 <mbilal.dev001@gmail.com>
2023-05-15 17:25:31 +0500

    Creator changes

--- src/store/user/reducers/UserReducer.tsx
+++ src/store/user/reducers/UserReducer.tsx
@@ -1,30 +1,23 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    isLogin: false,
-    token: "",
-    user: {}
+  isLogin: false,
+  token: '',
+  user: {},
 };
 
 const UserReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.USER_LOGIN:
-            return {
-                ...state,
-                isLogin: true,
-                token: action.payload.data.token,
-                user: action.payload.data.data.user
-            };
-        case actionTypes.USER_REGISTER:
-            return {
-                ...state,
-                isLogin: true,
-                token: action.payload.data.token,
-                user: action.payload.data.data.user
-            };
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case actionTypes.USER_LOGIN:
+      return {
+        ...state,
+        isLogin: true,
+        token: action.payload.data.token,
+        user: action.payload.data.data.user,
+      };
+    default:
+      return state;
+  }
+};
 
 export default UserReducer;
